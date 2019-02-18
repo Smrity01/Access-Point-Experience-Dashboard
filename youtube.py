9dc30d285f7d86efb3282dd522249f31f793019c
Smrity chaudhary <smrity.mca17.du@gmail.com>
2019-02-18 20:23:06 +0530

    converted into classes

--- youtube.py
+++ youtube.py
@@ -1,7 +1,6 @@
 '''
-Problems- 1.Unable to add the data to a csv file
-2.Unable to retrieve location
-3.Works for a single video id at a time
+Problems- 
+- Works for a single video id at a time
 
 '''
 from apiclient.discovery import build
@@ -15,72 +14,83 @@ import csv
 #from importlib import reload
 #sys.setdefaultencoding('utf8') 
 
-DEVELOPER_KEY = "AIzaSyC7YZD2osLIZ4GXFEMnoOdvQ6Hkr6mUcUs"
-YOUTUBE_API_SERVICE_NAME = "youtube"
-YOUTUBE_API_VERSION = "v3"
-pafy.set_api_key("AIzaSyC7YZD2osLIZ4GXFEMnoOdvQ6Hkr6mUcUs")
-
-def add_data(vID,title,description,author, likes,dislikes,rating,category,comments):
-	data = [vID,title,description,author,likes, dislikes,rating,category,comments]
-	#with open("scraper.csv", "a") as fp:
-		#wr = csv.writer(fp, dialect='excel')
-		#wr.writerow(data)
-	print(data) 
-		
-
-youtube = build(YOUTUBE_API_SERVICE_NAME, YOUTUBE_API_VERSION,developerKey=DEVELOPER_KEY)
-
-videoId = "GXGN4f6ma4k"
-url = "https://www.youtube.com/watch?v=" + videoId
-#Request fro Metadata of the Video
-video = pafy.new(url)
-
-#Request for Comments
-results = youtube.commentThreads().list(
+class Youtube():
+	def __init__(self):
+		developerKey = "AIzaSyC7YZD2osLIZ4GXFEMnoOdvQ6Hkr6mUcUs"
+		youtubeApiServiceName = "youtube"
+		youtubeApiVersion = "v3"
+		pafy.set_api_key("AIzaSyC7YZD2osLIZ4GXFEMnoOdvQ6Hkr6mUcUs")
+		self.youtube = build(youtubeApiServiceName, youtubeApiVersion, developerKey = developerKey)
+	def getVideoData(self, videoId):
+		url = "https://www.youtube.com/watch?v=" + videoId
+		#Request fro Metadata of the Video
+		videoData = pafy.new(url)
+		#Request for Comments
+		results = self.youtube.commentThreads().list(
 		    part="snippet",
 		    maxResults=100,
 		    videoId=videoId,
 		    textFormat="plainText"
 		  ).execute()
-totalResults = 0
-totalResults = int(results["pageInfo"]["totalResults"])
-count = 0
-nextPageToken = ''
-comments = []
-further = True
-first = True
-while further:
-	halt = False
-	if first == False:
-		print (".")
-		try:
-	  		results = youtube.commentThreads().list(
-	  		  part="snippet",
-	  		  maxResults=100,
-	  		  videoId=videoId,
-	  		  textFormat="plainText",
-	  		  pageToken=nextPageToken
-	  		).execute()
-	  		totalResults = int(results["pageInfo"]["totalResults"])
-		except HttpError as e:
-			halt = True
-	if halt == False:
-		count += totalResults
-		for item in results["items"]:
-			comment = item["snippet"]["topLevelComment"]
-			author = comment["snippet"]["authorDisplayName"]
-			text = comment["snippet"]["textDisplay"]
-			comments.append([author,text])
-		if totalResults < 100:
-			further = False
-			first = False
-		else:
-			further = True
-			first = False
-			try:
-				nextPageToken = results["nextPageToken"]
-			except KeyError as e:
+		totalResults = 0
+		totalResults = int(results["pageInfo"]["totalResults"])
+		count = 0
+		nextPageToken = ''
+		comments = []
+		further = True
+		first = True
+		while further:
+			halt = False
+			if first == False:
+				print (".")
+				try:
+					results = self.youtube.commentThreads().list(part = "snippet", maxResults = 100, videoId = videoId, textFormat = "plainText", pageToken = nextPageToken).execute()
+					totalResults = int(results["pageInfo"]["totalResults"])
+				except HttpError as e:
+					halt = True
+			if halt == False:
+				count += totalResults
+				for item in results["items"]:
+					comment = item["snippet"]["topLevelComment"]
+					author = comment["snippet"]["authorDisplayName"]
+					text = comment["snippet"]["textDisplay"]
+					comments.append([author,text])
+			if totalResults < 100:
 				further = False
-
+				first = False
+			else:
+				further = True
+				first = False
+				try:
+					nextPageToken = results["nextPageToken"]
+				except KeyError as e:
+					further = False
+		return comments
+	def getUserData(self, comments):
+		hi = []
+		for comment in comments:
+			hi.append(self.youtube.channels().list(part = 'snippet',forUsername=comment[0]).execute())
+			print(comment[0],'\n',comment[1])
+		for i in hi:
+			print(i,'\n\n')
+'''
+print (yObject.youtube.channels().list(part = 'snippet',forUsername='kentuckyrangerpro').execute())
+#print(comments)
+for item in results['items']:
+	#print(type(item))
+	print(item['items']['kind'])
+	# ,"\n",item['snippet']['authorDisplayName']) 
+#print(results[items])
+'''    
 # Adding the full data to CSV
-add_data(videoId,video.title,video.description,video.author,video.likes, video.dislikes,video.rating,video.category,comments)
+#print(videoId,video.title,video.description,video.author,video.likes, video.dislikes,video.rating,video.category,comments)
+
+def main():
+	videoId = "GXGN4f6ma4k"
+	yObject = Youtube()
+	comments = yObject.getVideoData(videoId)
+	yObject.getUserData(comments)
+
+if __name__ == "__main__":
+    # calling main function
+    main()

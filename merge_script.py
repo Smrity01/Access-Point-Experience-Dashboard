d7b0abacb0e209777280744cfc37846e407dea99
Smrity chaudhary <smrity.mca17.du@gmail.com>
2019-06-14 21:46:34 +0530

    UPDATED

--- merge_script.py
+++ merge_script.py
@@ -1,19 +1,32 @@
 import pandas
-
-colnames = ['Tweet','Location','Positive','Negative']
+import numpy
+import random
+from datetime import datetime
 #Twitter
-tweets = pandas.read_csv('tweety.csv',usecols = colnames)
-tweets.insert(0,'Type','Twitter') #Inserting Type = 'Twitter' at the Beginning of the Data
 
-#Youtube_Review
-youtube_r = pandas.read_csv('review.csv',usecols = colnames) 
-youtube_r.insert(0,'Type','Youtube') #Inserting Type = 'Youtube' at Beginning of the Data
+twitterLastUpdatedDate = '2018-06-13 02:58:58'
+tweets = pandas.read_csv('tweety.csv')
+tweets = tweets[['Comment','Location','UserId','Compound','Negative','Neutral','Positive','PublishedDate']]
+tweets1 = tweets[tweets['PublishedDate'] == " "]
+tweets = tweets[tweets['PublishedDate'] > twitterLastUpdatedDate]
+#print(tweets1)
+#print(tweets['PublishedDate'])
+#Youtube_Review original
+youtubeLastUpdatedDate = '2010-06-12 19:45:08'
+youtubeReview = pandas.read_csv('review.csv') 
+youtubeReview = youtubeReview[['Comment','Location','UserId','Compound','Negative','Neutral','Positive','PublishedDate']]
+youtubeReview = youtubeReview[youtubeReview['PublishedDate'] > youtubeLastUpdatedDate]
+#print(youtubeReview)
+#youtubeReview = youtubeReview[youtubeReview['PublishedDate'] > youtubeLastUpdatedDate]
 
-#Youtube_Output
-youtube_o = pandas.read_csv('output.csv',usecols = colnames) 
-youtube_o.insert(0,'Type','Youtube') #Inserting Type = 'Youtube' at Beginning of the Data
 
+#Youtube_manipulated data
+youtubeLastUpdatedDate2 = '2010-06-12 19:45:08'
+countries = [ 'US' , 'UK','GB' ,'MX' ,'SE' ,'DK','DE']
+youtubeManipulated = pandas.read_csv('output.csv') 
+youtubeManipulated = youtubeManipulated[['Comment','Location','UserId','Compound','Negative','Neutral','Positive','PublishedDate']]
+youtubeManipulated['Location'] = youtubeManipulated['Location'].apply(lambda x:random.choice(countries))
+youtubeManipulated = youtubeManipulated[youtubeManipulated['PublishedDate'] > youtubeLastUpdatedDate2]
 
-final_data = pandas.concat([tweets,youtube_r])
-final_data = pandas.concat([final_data,youtube_o])
-final_data.to_csv('Test.csv',index=False)
+mergedData = pandas.concat([tweets1,tweets,youtubeReview, youtubeManipulated], ignore_index = False)
+mergedData.to_csv('lockerData.csv',index=False)
